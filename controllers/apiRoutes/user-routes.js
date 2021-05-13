0cdec97ca092e24309b3d80a681a44cf19b49ccb
Sydney Walcoff <sydney.walcoff@gmail.com>
2021-05-13 11:17:47 -0700

    add session info to post new user route

--- controllers/apiRoutes/user-routes.js
+++ controllers/apiRoutes/user-routes.js
@@ -68,14 +68,22 @@ router.post('/login', (req, res) => {
 	});
   });
 
-// Post Users
+// add new user
 router.post('/', (req, res) => {
 	User.create({
 		username: req.body.username,
 		email: req.body.email,
 		password: req.body.password
 	})
-	.then(dbUserData => res.json(dbUserData))
+	.then(dbUserData => {
+		req.session.save(() => {
+            req.session.user_id = dbUserData.id;
+            req.session.username = dbUserData.username;
+            req.session.loggedIn = true;
+
+            res.json(dbUserData);
+        });
+	})
 	.catch(err => {
 		console.log(err);
 		res.status(500).json(err);
