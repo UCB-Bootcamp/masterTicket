b4485dba5c6320cfd7f88cdf087ac7182eae9eaa
Sydney Walcoff <sydney.walcoff@gmail.com>
2021-05-13 11:38:35 -0700

    add logout route

--- controllers/apiRoutes/user-routes.js
+++ controllers/apiRoutes/user-routes.js
@@ -73,6 +73,17 @@ router.post('/login', (req, res) => {
 	});
 });
 
+// logout
+router.post('/logout', (req, res) => {
+    if(req.session.loggedIn) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    } else {
+        res.status(404).end();
+    }
+});
+
 // add new user
 router.post('/', (req, res) => {
 	User.create({
